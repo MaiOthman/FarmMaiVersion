96f9529fdafd5f3a1fd76ad4565a720592b1a4c6
MaiOthman <mai.m.othman94@gmail.com>
2025-02-14 11:29:55 +0100

    Initial Commit

---
+++ FarmTeam/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FarmTeam
+//
+//  Created by Mai Mahmoud on 14.02.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
